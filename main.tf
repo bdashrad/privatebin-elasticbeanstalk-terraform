e2beccb6a633ff58f81ed08311d914eb7aeb7f4f
Brad Clark <bdashrad@gmail.com>
2016-09-29 00:10:04 -0400

    build EB app, env, and dns records

--- main.tf
+++ main.tf
@@ -1,5 +1,63 @@
 # main
 provider "aws" {
-  region = "${var.region}"
+  region = "${var.aws_region}"
+}
+
+resource "aws_elastic_beanstalk_application" "privatebin" {
+  name = "privatebin"
+  description = "minimalist, open source online pastebin"
+}
+
+resource "aws_elastic_beanstalk_environment" "privatebin-prod" {
+  name = "prod"
+  application = "${aws_elastic_beanstalk_application.privatebin.name}"
+  solution_stack_name = "64bit Amazon Linux 2016.03 v2.1.6 running PHP 7.0"
+
+  # Instance type
+  setting = {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name = "InstanceType"
+    value = "m3.medium"
+  }
+
+  # Load Balancing
+  setting = {
+    namespace = "aws:elb:listener:443"
+    name = "ListenerProtocol"
+    value = "HTTPS"
+  }
+
+  setting = {
+    namespace = "aws:elb:listener:443"
+    name = "InstancePort"
+    value = "80"
+  }
+
+  setting = {
+    namespace = "aws:elb:listener:443"
+    name = "SSLCertificateId"
+    value = "arn:aws:acm:${var.aws_region}:${var.aws_account_id}:certificate/${var.ssl_cert_id}"
+  }
+
+  setting = {
+    namespace = "aws:elb:loadbalancer"
+    name = "CrossZone"
+    value = "true"
+  }
+
+  setting = {
+    namespace = "aws:elb:policies"
+    name = "ConnectionDrainingEnabled"
+    value = "true"
+  }
+
+}
+
+resource "aws_route53_record" "privatebin" {
+  zone_id = "${var.zone_id}"
+  name = "bin"
+  type = "CNAME"
+  ttl = "60"
+  records = ["aws_elastic_beanstalk_environment.privatebin-prod.cname"]
 }
 
--- variables.tf
+++ variables.tf
@@ -1,5 +1,20 @@
-variable "region" {
+variable "aws_account_id" {
+  type = "string"
+  description = "aws account id, used to generate some ARNs"
+}
+
+variable "aws_region" {
   type = "string"
   description = "AWS region to host app in"
   default = "us-east-1"
+}
+
+variable "ssl_cert_id" {
+  type = "string"
+  description = "certificate ID from amazon certificate manager for ELB"
+}
+
+variable "zone_id" {
+  type = "string"
+  description = "zone identifier of the route53 zone where the dns record will live"
 }
